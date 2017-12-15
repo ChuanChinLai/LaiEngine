5159fbd1113c3a621e01afea484cdf3c5293f5ac
danny6430 <danny6430@hotmail.com>
2017-12-14 22:35:57 -0700

    working on AssetBuilder

--- External/Lua/Includes.h
+++ External/Lua/Includes.h
@@ -1,10 +1,4 @@
-/*
-	This file can be included by a project that wants to use Lua
-*/
-
-#ifndef EAE6320_LUAINCLUDES_H
-#define EAE6320_LUAINCLUDES_H
+#pragma once
 
 #include "5.3.4/src/lua.hpp"
 
-#endif	// EAE6320_LUAINCLUDES_H
--- Tools/AssetBuildLibrary/AssetBuildFunctions.lua
+++
@@ -1,9 +0,0 @@
---[[
-	This file contains the logic for building assets
-]]
-
--- Static Data Initialization
---===========================
-
-ExamplePrint( "Example string to print" )
-ExamplePrint( 123.456 )
