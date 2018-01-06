ff3d4347a9ea25be4b02bdad9bd5cf2a07aec8f6
ChuanChinLai <chuanchinlai@gmail.com>
2018-01-06 06:34:38 -0700

    add test

--- ExampleGame_/ExampleGame/main.cpp
+++ ExampleGame_/ExampleGame/main.cpp
@@ -3,8 +3,89 @@
 #include <ExampleGame_\ExampleGame\EngineDemo.h>
 #include <Engine\Math\Vector4D.h>
 
+class base
+{
+public:
+
+	base()
+	{
+		std::cout << "base constructor" << std::endl;
+	}
+	virtual ~base()
+	{
+		std::cout << "base deconstructor" << std::endl;
+	}
+	void call()
+	{
+		std::cout << "base call" << std::endl;
+	}
+
+	virtual void vcall()
+	{
+		std::cout << "base vcall" << std::endl;
+	}
+};
+
+
+class derived : public base
+{
+public:
+
+	derived()
+	{
+		std::cout << "derived constructor" << std::endl;
+	}
+
+	~derived()
+	{
+		std::cout << "derived deconstructor" << std::endl;
+	}
+
+	void call()
+	{
+		std::cout << "derived call" << std::endl;
+	}
+
+	virtual void vcall() override
+	{
+		std::cout << "derived vcall" << std::endl;
+	}
+};
+
+void test(void* par)
+{
+	base* c = static_cast<base*>(par);
+	c->call();
+	c->vcall();
+}
+
+
 int main(int argc, char *args[])
 {
+	{
+		{
+			derived b;
+			b.call();
+			b.vcall();
+		}
+
+		{
+			base* b = new derived();
+			b->call();
+			b->vcall();
+
+			test(b);
+
+
+			void *a = b;
+			base* c = static_cast<base*>(a);
+			c->call();
+			c->vcall();
+
+			delete b;
+		}
+	}
+
 
 	Engine::EngineDemo* Game = new Engine::EngineDemo();
 
