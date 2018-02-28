3d019b17415c950a9427e3d50bf591be49e078bb
ChuanChinLai <chuanchinlai@gmail.com>
2018-02-27 23:47:41 -0700

    update header in the project

--- Engine/Memory/SmartPtr/SharedPointer.h
+++ Engine/Memory/SmartPtr/SharedPointer.h
@@ -14,64 +14,65 @@ namespace Engine
         {
         public:
             
-			//Constructor:
-			shared_ptr() : m_ptr(nullptr), m_pRefCounter(nullptr)
-			{
+            //Constructor:
+            shared_ptr() : m_ptr(nullptr), m_pRefCounter(nullptr)
+            {
 
-			}
+            }
 
-			explicit shared_ptr(T* i_ptr) : m_ptr(i_ptr), m_pRefCounter(nullptr)
-			{
-				if (m_ptr != nullptr)
-					m_pRefCounter = new RefCounter(1, 0);
-			}
+            explicit shared_ptr(T* i_ptr) : m_ptr(i_ptr), m_pRefCounter(nullptr)
+            {
+                if (m_ptr != nullptr) 
+                    m_pRefCounter = new RefCounter(1, 0);
+            }
 
-			~shared_ptr()
-			{
-				_Release();
-			}
+            ~shared_ptr()
+            {
+                _Release();
+            }
 
-			//Copy Constructor: 
+            //Copy Constructor: 
 			
-			shared_ptr(const shared_ptr& i_SharedPtr) : m_ptr(i_SharedPtr.m_ptr), m_pRefCounter(i_SharedPtr.m_pRefCounter)
-			{
-				_Acquire();
-			}
+            shared_ptr(const shared_ptr& i_SharedPtr) : m_ptr(i_SharedPtr.m_ptr), m_pRefCounter(i_SharedPtr.m_pRefCounter)
+            {
+                _Acquire();
+            }
 
-			shared_ptr(shared_ptr&& i_SharedPtr) : m_ptr(i_SharedPtr.m_ptr), m_pRefCounter(i_SharedPtr.m_pRefCounter)
-			{
-				i_SharedPtr.m_ptr = nullptr;
-				i_SharedPtr.m_pRefCounter = nullptr;
-			}
+            shared_ptr(shared_ptr&& i_SharedPtr) : m_ptr(i_SharedPtr.m_ptr), m_pRefCounter(i_SharedPtr.m_pRefCounter)
+            {
+                i_SharedPtr.m_ptr = nullptr;
+                i_SharedPtr.m_pRefCounter = nullptr;
+            }
 
-			//Assignment Operator: 
-			inline shared_ptr<T>& operator=(const shared_ptr &i_SharedPtr);
-			inline shared_ptr<T>& operator=(shared_ptr&& i_SharedPtr);
+            //Assignment Operator: 
+            inline shared_ptr<T>& operator = (const shared_ptr &i_SharedPtr);
+            inline shared_ptr<T>& operator = (shared_ptr&& i_SharedPtr);
 
-			//Overloaded Operator: 
-			inline T& operator* () const;
-			inline T* operator->() const;
+            //Overloaded Operator: 
+            inline T& operator* () const;
+            inline T* operator->() const;
 
-			//Operators:
-			inline operator bool() const;
+            //Operators:
+            inline operator bool() const;
 
-			inline bool operator==(const shared_ptr& i_SharedPtr) const;
-			inline bool operator!=(const shared_ptr& i_SharedPtr) const;
+            inline bool operator == (const shared_ptr& i_SharedPtr) const;
+            inline bool operator != (const shared_ptr& i_SharedPtr) const;
 
-			inline   T* _Get() const;
-
-		private:
-			inline void _Acquire();
-			inline void _Release();
+            inline T* _Get() const;
 
         private:
-            
+
+            inline void _Acquire();
+            inline void _Release();
+
+        private:
+			
             T*              m_ptr;
             RefCounter*     m_pRefCounter;
 
-			friend class weak_ptr<T>;
+            friend class weak_ptr<T>;
         };
-	}
+    }
 }
 
 
--- Engine/Memory/SmartPtr/SharedPointer_inline.h
+++ Engine/Memory/SmartPtr/SharedPointer_inline.h
@@ -5,104 +5,102 @@
 
 namespace Engine
 {
-	namespace Memory
-	{
-		template<class T>
-		inline shared_ptr<T> & shared_ptr<T>::operator=(const shared_ptr & i_SharedPtr)
+    namespace Memory
+    {
+        template<class T>
+        inline shared_ptr<T>& shared_ptr<T>::operator = (const shared_ptr & i_SharedPtr)
+        {
+            if (*this != i_SharedPtr)
+            {
+                _Release();
+
+                m_ptr = i_SharedPtr.m_ptr;
+                m_pRefCounter = i_SharedPtr.m_pRefCounter;
+
+               _Acquire();
+            }
+            return *this;
+        }
+
+        template<class T>
+        inline shared_ptr<T>& shared_ptr<T>::operator = (shared_ptr && i_SharedPtr)
+        {
+            if (*this != i_SharedPtr)
+            {
+                std::swap(m_ptr, i_SharedPtr.m_ptr);
+                std::swap(m_pRefCounter, i_SharedPtr.m_pRefCounter);
+            }
+
+            return *this;
+        }
+
+        template<class T>
+        inline T& shared_ptr<T>::operator * () const
+        {
+            assert(m_ptr != nullptr);
+            return *m_ptr;
+        }
+
+        template<class T>
+        inline T* shared_ptr<T>::operator -> () const
+        {
+            return m_ptr;
+        }
+
+        template<class T>
+        inline shared_ptr<T>::operator bool() const
 		{
-			if (*this != i_SharedPtr)
-			{
-				_Release();
+            return m_ptr != nullptr;
+        }
 
-				m_ptr = i_SharedPtr.m_ptr;
-				m_pRefCounter = i_SharedPtr.m_pRefCounter;
-
-				_Acquire();
-			}
-			return *this;
-		}
-
-		template<class T>
-		inline shared_ptr<T> & shared_ptr<T>::operator=(shared_ptr && i_SharedPtr)
+        template<class T>
+        inline bool shared_ptr<T>::operator == (const shared_ptr& i_SharedPtr) const
 		{
-			if (*this != i_SharedPtr)
-			{
-				std::swap(m_ptr, i_SharedPtr.m_ptr);
-				std::swap(m_pRefCounter, i_SharedPtr.m_pRefCounter);
-			}
+            return m_ptr == i_SharedPtr.m_ptr;
+        }
 
-			return *this;
-		}
+        template<class T>
+        inline bool shared_ptr<T>::operator!=(const shared_ptr& i_SharedPtr) const
+        {
+            return m_ptr != i_SharedPtr.m_ptr;
+        }
 
-		template<class T>
-		inline T & shared_ptr<T>::operator*() const
-		{
-			assert(m_ptr != nullptr);
-			return *m_ptr;
-		}
-
-		template<class T>
-		inline T * shared_ptr<T>::operator->() const
-		{
-			return m_ptr;
-		}
-
-		template<class T>
-		inline shared_ptr<T>::operator bool() const
-		{
-			return m_ptr != nullptr;
-		}
-
-		template<class T>
-		inline bool shared_ptr<T>::operator==(const shared_ptr & i_SharedPtr) const
-		{
-			return m_ptr == i_SharedPtr.m_ptr;
-		}
-
-		template<class T>
-		inline bool shared_ptr<T>::operator!=(const shared_ptr & i_SharedPtr) const
-		{
-			return m_ptr != i_SharedPtr.m_ptr;
-		}
-
+        template<class T>
+        inline T * shared_ptr<T>::_Get() const
+        {
+            return m_ptr;
+        }
 		
-		template<class T>
-		inline T * shared_ptr<T>::_Get() const
-		{
-			return m_ptr;
-		}
-		
-		template<class T>
-		inline void shared_ptr<T>::_Acquire()
-		{
-			if (m_pRefCounter != nullptr)
-			{
-				m_pRefCounter->SharedCount++;
-			}
-		}
+        template<class T>
+        inline void shared_ptr<T>::_Acquire()
+        {
+            if (m_pRefCounter != nullptr)
+            {
+                m_pRefCounter->SharedCount++;
+            }
+        }
 
-		template<class T>
-		inline void shared_ptr<T>::_Release()
-		{
-			if (m_pRefCounter != nullptr)
-			{
-				m_pRefCounter->SharedCount--;
+        template<class T>
+        inline void shared_ptr<T>::_Release()
+        {
+            if (m_pRefCounter != nullptr)
+            {
+                m_pRefCounter->SharedCount--;
 
-				if (m_pRefCounter->SharedCount <= 0 && m_pRefCounter->WeakCount <= 0)
-				{
-					delete m_ptr;
-					m_ptr = nullptr;
+                if (m_pRefCounter->SharedCount <= 0 && m_pRefCounter->WeakCount <= 0)
+                {
+                    delete m_ptr;
+                    m_ptr = nullptr;
 
-					delete m_pRefCounter;
-					m_pRefCounter = nullptr;
-				}
-				else if (m_pRefCounter->SharedCount <= 0)
-				{
-					delete m_ptr;
-					m_ptr = nullptr;
-				}
-			}
-		}
-
-	}
+                    delete m_pRefCounter;
+                    m_pRefCounter = nullptr;
+                }
+                else if (m_pRefCounter->SharedCount <= 0)
+                {
+                    delete m_ptr;
+                    m_ptr = nullptr;
+                }
+            }
+        }
+    }
 }
